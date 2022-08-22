6dee724a01e554eee2f569fe874e0ee65320244a
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-08-22 22:12:00 +0530

    initial commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Saurabh Jamadagni on 22/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Saurabh Jamadagni on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
