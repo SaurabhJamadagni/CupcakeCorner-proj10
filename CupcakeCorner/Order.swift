68ee43f77c04ab23d220fd12b092967dff654f7e
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-09-01 00:35:08 +0530

    code: adding networking calls and core functionality

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -7,12 +7,16 @@
 
 import SwiftUI
 
-class Order: ObservableObject {
+class Order: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case type, qunatity, hasSpecialRequest, hasExtraFrosting, wantsSprinkles, name, streetAddress, city, zip
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
     @Published var type = 0
     @Published var quantity = 3
-    @Published var hasSpecialRequest = true {
+    @Published var hasSpecialRequest = false {
         // Required so that these revert back to false when special request is toggled off.
         didSet {
             if !hasSpecialRequest {
@@ -52,4 +56,36 @@ class Order: ObservableObject {
         
         return cost
     }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .qunatity)
+        try container.encode(hasSpecialRequest, forKey: .hasSpecialRequest)
+        try container.encode(hasExtraFrosting, forKey: .hasExtraFrosting)
+        try container.encode(wantsSprinkles, forKey: .wantsSprinkles)
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .qunatity)
+        hasSpecialRequest = try container.decode(Bool.self, forKey: .hasSpecialRequest)
+        hasExtraFrosting = try container.decode(Bool.self, forKey: .hasExtraFrosting)
+        wantsSprinkles = try container.decode(Bool.self, forKey: .wantsSprinkles)
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+    }
+    
+    // Empty initializer because otherwise we can't create an empty order.
+    // Empty order required at the start of the app.
+    init() { }
 }
